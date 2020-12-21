842c6127f549a087c27cfbb2760591dfffbea535
Aline Alves <alinefsalvessi@gmail.com>
2020-12-21 10:31:04 -0300

    Incluisao Modulo Autenticacao

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({extended: false}));
 
-require('./controllers/authControler')(app);
+require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 /*
 app.get('/', (req, res) => {
